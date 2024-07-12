5831564dcf83e08aa26bd1efb2c4778270df6824
Akseenja <klasik1995@bk.ru>
2024-07-12 19:26:15 +0300

    refactor: change class App component to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,61 @@
 import './App.css';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Results } from './components/pages/Results';
-import Loader from './utils/Loader';
 import { ResultItem } from './services/types';
 import { Search } from './components/Search';
+import { fetchAllPages } from './services/api';
 
 type AppState = {
   searchData: ResultItem[];
   query: string;
-  loading: boolean;
 };
 
-type ApiResponse = {
-  results: ResultItem[];
-  next: string | null;
+const initialState: AppState = {
+  searchData: [],
+  query: ''
 };
 
-class App extends React.Component<Record<string, unknown>, AppState> {
-  constructor(props: Record<string, unknown>) {
-    super(props);
-    this.state = {
-      loading: false,
-      searchData: [],
-      query: ''
-    };
-  }
+export const App = () => {
+  const [state, setState] = useState(initialState);
 
-  componentDidMount(): void {
+  useEffect(() => {
     const savedQuery = localStorage.getItem('searchQuery');
     if (savedQuery) {
-      this.setState({ query: savedQuery });
-      this.fetchData(savedQuery);
+      setState((prevState) => ({
+        ...prevState,
+        query: savedQuery
+      }));
+      fetchData(savedQuery);
     } else {
-      this.fetchData('');
+      fetchData('');
     }
-  }
+  }, []);
 
-  fetchAllPages = async (url: string): Promise<ResultItem[]> => {
-    let allResults: ResultItem[] = [];
-    let currentPageUrl: string | null = url;
-
-    while (currentPageUrl) {
-      try {
-        const response: Response = await fetch(currentPageUrl);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data: ApiResponse = await response.json();
-
-        if (data.results && Array.isArray(data.results)) {
-          allResults = allResults.concat(data.results);
-        }
-
-        currentPageUrl = data.next;
-      } catch (error) {
-        console.error('Fetch error: ', error);
-        break;
-      }
-    }
-
-    return allResults;
-  };
-
-  fetchData = async (query: string): Promise<void> => {
-    this.setState({ loading: true });
-
+  const fetchData = async (query: string): Promise<void> => {
     try {
-      const allResults = await this.fetchAllPages(
-        'https://swapi.dev/api/people/'
-      );
-      const filteredResults = allResults.filter((item) =>
+      const allResults = await fetchAllPages('https://swapi.dev/api/people/');
+      const filteredResults = allResults.filter((item: ResultItem) =>
         item.name.toLowerCase().includes(query.toLowerCase())
       );
-
-      this.setState({ searchData: filteredResults, loading: false });
+      setState((prevState) => ({
+        ...prevState,
+        searchData: filteredResults
+      }));
     } catch (error) {
       console.error('Fetch error: ', error);
-      this.setState({ searchData: [], loading: false });
+      setState({ ...state, searchData: [] });
     }
   };
 
-  render() {
-    const { searchData } = this.state;
-    return (
-      <div>
-        <section className='section__search'>
-          <Search fetchData={this.fetchData} />
-        </section>
-        <section className='section__result'>
-          <h2>Data Base:</h2>
-          {this.state.loading ? <Loader /> : <Results data={searchData} />}
-        </section>
-      </div>
-    );
-  }
-}
-
-export default App;
+  return (
+    <div>
+      <section className='section__search'>
+        <Search fetchData={fetchData} />
+      </section>
+      <section className='section__result'>
+        <h2>Data Base:</h2>
+        <Results data={state.searchData} />
+      </section>
+    </div>
+  );
+};
