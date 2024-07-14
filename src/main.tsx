14ad63e55981f89c7020f83a9dc8a34f1d3a7582
Akseenja <klasik1995@bk.ru>
2024-07-15 01:59:29 +0300

    fix: unneeded router file deleted

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import './App.css';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
